a14688a956246287bf02d21522064329d877e8c7
@guustavocl <gustavo@gus.sh>
2023-05-29 15:42:57 -0300

    missing lights class on stack

--- src/components/Home/Technologies.tsx
+++ src/components/Home/Technologies.tsx
@@ -54,7 +54,7 @@ const Technologies = () => {
           <Icon Component={SiAndroidstudio} tooltip="Android Studio" className="display md:hidden" />
         </div>
 
-        <span className="absolute bottom-2 right-4 w-full -space-y-1 text-right">
+        <span className="lights absolute bottom-2 right-4 w-full -space-y-1 text-right">
           <span className="block font-mono text-2xl font-bold text-pink-200 text-glow-violet-500 sm:text-xl">
             MY STACK
           </span>
