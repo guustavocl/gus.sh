e80dfa3d27f292c94ef07c3c40ae48a6e4f231d0
guustavocl <gustavo@gus.sh>
2023-10-16 14:37:34 -0300

    preparing to app directory

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    domains: ["cdn.discordapp.com", "i.scdn.co", "live.staticflickr.com", "api.mapbox.com"],
-  },
-};
-
-module.exports = nextConfig;
--- src/utils/constants.ts
+++
@@ -1,6 +0,0 @@
-export const BRTimeFormatter = new Intl.DateTimeFormat(undefined, {
-  timeZone: "America/Sao_Paulo",
-  hour: "numeric",
-  minute: "numeric",
-  hour12: false,
-});
