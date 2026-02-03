d957ae70a37b9a1cac576a6610b0cb5f64b48d15
guustavocl <gus@gus.sh>
2026-02-03 13:22:22 -0300

    fixing vercel Analytics

--- app/components/Analytics.tsx
+++ app/components/Analytics.tsx
@@ -1,12 +1,10 @@
 import Clarity from "@microsoft/clarity";
-import { inject } from "@vercel/analytics";
+import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 import { posthog } from "posthog-js";
 import { useEffect } from "react";
 
 const Analytics = ({ PH_TRACKING_ID, MS_CLARITY_ID }: { PH_TRACKING_ID?: string; MS_CLARITY_ID?: string }) => {
   useEffect(() => {
-    inject();
-
     if (PH_TRACKING_ID) {
       posthog.init(PH_TRACKING_ID, {
         api_host: "https://app.posthog.com",
@@ -18,7 +16,7 @@ const Analytics = ({ PH_TRACKING_ID, MS_CLARITY_ID }: { PH_TRACKING_ID?: string;
     }
   }, [PH_TRACKING_ID, MS_CLARITY_ID]);
 
-  return null;
+  return <VercelAnalytics />;
 };
 
 export default Analytics;
